8d47fa9917819be999e0c25b8d329273ed71ffad
CalculatingZero <20590197+CalculatingZero@users.noreply.github.com>
2023-12-29 14:38:48 -0800

    update nft-fetcher

--- src/MixtapeSearch.tsx
+++ src/MixtapeSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { getMixtapeNFTs } from 'nft-fetcher';
+import getMixtapeNFTs from 'nft-fetcher';
 import styles from './MixtapeSearch.module.css';
 import { CID } from 'multiformats/cid'; 
 
